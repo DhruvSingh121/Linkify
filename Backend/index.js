14b94951464c1c4877a12a2d973f61e83fc69341
Dhruvsingh121 <GodZeusOP@gmail.com>
2026-04-11 21:49:20 +0530

    Connecting With Frontend

--- Backend/index.js
+++ Backend/index.js
@@ -1,15 +1,22 @@
 import express from "express";
 import dotenv from "dotenv";
-dotenv.config();
+import cors from "cors"; // ← ADD THIS
 import urlRoute from "./routes/url.js";
 import { connectDB } from "./connect.js";
+
+dotenv.config();
 const app = express();
 
 connectDB(process.env.MONGO_URI)
-  .then(console.log("MongoDb Connected"))
+  .then(console.log("MongoDB Connected"))
   .catch((err) => console.log("DB ERROR:", err));
 
 app.use(express.json());
+app.use(
+  cors({
+    origin: "*",
+  }),
+);
 app.use("/url", urlRoute);
 
 app.listen(process.env.PORT, () => {
