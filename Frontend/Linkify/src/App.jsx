33df849a16f83d2d71be1680faf2692bee689c82
Dhruvsingh121 <GodZeusOP@gmail.com>
2026-04-13 17:00:09 +0530

    UI Improved + Big Fixed

--- Frontend/Linkify/src/App.jsx
+++ Frontend/Linkify/src/App.jsx
@@ -5,16 +5,6 @@ import "react-toastify/dist/ReactToastify.css";
 function App() {
   return (
     <div className="app">
-      <ToastContainer
-        position="top-center"
-        autoClose={3000}
-        hideProgressBar={false}
-        newestOnTop
-        closeOnClick
-        pauseOnHover
-        draggable
-        theme="dark"
-      />
       <Home />
     </div>
   );
